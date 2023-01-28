d0014959598cd7f0eebd6df5cf70b4c8d3047d19
Bryance Oyang <bryance@ducky>
2023-01-28 14:03:18 -0800

    checkpoint

--- ws_ctube.c
+++ ws_ctube.c
@@ -11,10 +11,9 @@
 #include "ws_base.h"
 
 #define WS_CTUBE_BUFLEN 4096
-typedef int new_conn_t;
 
 struct poll_list {
-	struct pollfd *fds;
+	int *fds;
 	nfds_t nfds;
 	nfds_t cap;
 	pthread_mutex_t mutex;
@@ -42,7 +41,7 @@ static void poll_list_add(struct poll_list *restrict pl, int fd)
 		pl->fds = realloc(pl->fds, pl->cap * sizeof(*pl->fds));
 	}
 
-	pl->fds[pl->nfds].fd = fd;
+	pl->fds[pl->nfds] = fd;
 	pl->nfds++;
 	pthread_mutex_unlock(&pl->mutex);
 }
@@ -52,7 +51,7 @@ static void poll_list_remove(struct poll_list *restrict pl, int fd)
 	pthread_mutex_lock(&pl->mutex);
 	nfds_t i;
 	for (i = 0; i < pl->nfds; i++) {
-		if (fd == pl->fds[i].fd) {
+		if (fd == pl->fds[i]) {
 			break;
 		}
 	}
@@ -70,8 +69,23 @@ static void poll_list_remove(struct poll_list *restrict pl, int fd)
 		pl->cap = half_cap;
 		pl->fds = realloc(pl->fds, pl->cap * sizeof(*pl->fds));
 	}
-
 	pthread_mutex_unlock(&pl->mutex);
+}
+
+static struct pollfd *poll_list_alloc_cpy(const struct poll_list *restrict pl)
+{
+	pthread_mutex_lock(&pl->mutex);
+	struct pollfd *fds = malloc(pl->nfds * sizeof(*fds));
+	if (fds == NULL) {
+		fprintf(stderr, "poll_list_cpy(): out of memory\n");
+		fflush(stderr);
+		return NULL;
+	}
+	for (nfds_t i = 0; i < pl->nfds; i++) {
+		fds[i].fd = pl->fds[i];
+	}
+	pthread_mutex_unlock(&pl->mutex);
+	return fds;
 }
 
 struct conn_handler_td {
@@ -79,47 +93,8 @@ struct conn_handler_td {
 	int serv_pipefd;
 };
 
-static void pollfds_init(struct pollfd *const fds, int data_ready_pipefd, int serv_pipefd)
-{
-	fds[0].fd = data_ready_pipefd;
-	fds[1].fd = serv_pipefd;
-}
-
-/** poll all for reading */
-static int poll_read(struct pollfd *const fds, nfds_t nfds)
-{
-	for (nfds_t i = 0; i < nfds; i++) {
-		fds[i].events = POLLIN;
-	}
-	return poll(fds, nfds, -1);
-}
-
-/** poll the non-pipe connections for writing */
-static int poll_write(struct pollfd *const fds, nfds_t nfds)
-{
-	fds[0].events = 0;
-	fds[1].events = 0;
-	for (nfds_t i = 2; i < nfds; i++) {
-		fds[i].events = POLLOUT;
-	}
-	return poll(fds, nfds, -1);
-}
-
 static void handle_data_ready(struct ws_ctube *restrict ctube, struct pollfd *restrict fds, nfds_t nfds)
 {
-	/* clear pipe buffer */
-	char buf;
-	read(ctube->_data_ready_pipefd[0], &buf, 1);
-
-	poll_write(fds, nfds);
-
-	ws_ctube_lock(ctube);
-	for (nfds_t i = 2; i < nfds; i++) {
-		if (fds->revents & POLLOUT) {
-			ws_send(fds->fd, ctube->data, ctube->data_size);
-		}
-	}
-	ws_ctube_unlock(ctube);
 }
 
 static void handle_income(int conn)
@@ -130,38 +105,10 @@ static void handle_income(int conn)
 
 static void handle_new_conn(struct pollfd *restrict *restrict const fds, nfds_t *restrict const nfds, char *const buf)
 {
-	int nread;
-	int ntoread = sizeof(new_conn_t);
-
-	char *pbuf = buf;
-	while (ntoread > 0) {
-		nread = read((*fds)[1].fd, pbuf, ntoread);
-		if (nread <= 0) {
-			return;
-		}
-		ntoread -= nread;
-		pbuf += ntoread;
-	}
-	if (ntoread != 0) {
-		fprintf(stderr, "handle_new_conn(): weird pipe read");
-		fflush(stderr);
-		raise(SIGSEGV);
-	}
-
-	new_conn_t conn;
-	memcpy(&conn, buf, sizeof(new_conn_t));
-
-	(*nfds)++;
-	*fds = realloc(*fds, *nfds * sizeof(**fds));
-	(*fds)[*nfds - 1].fd = conn;
 }
 
 static void handle_bad_conn(struct pollfd *restrict const fds, nfds_t *restrict const nfds, nfds_t i)
 {
-	if (i < *nfds - 1) {
-		memmove(&fds[i], &fds[i + 1], (*nfds - i - 1) * sizeof(*fds));
-	}
-	(*nfds)--;
 }
 
 /** main for connection handler */
@@ -170,54 +117,6 @@ static void *conn_handler_main(void *arg)
 	struct ws_ctube *ctube = ((struct conn_handler_td *)arg)->ctube;
 	int serv_pipefd = ((struct conn_handler_td *)arg)->serv_pipefd;
 
-	nfds_t nfds = 2;
-	struct pollfd *fds;
-
-	fds = malloc(nfds * sizeof(*fds));
-	if (fds == NULL) {
-		goto out_no_fds;
-	}
-	pollfds_init(fds, ctube->_data_ready_pipefd[0], serv_pipefd);
-
-	char buf[WS_CTUBE_BUFLEN];
-	if (WS_CTUBE_BUFLEN < sizeof(new_conn_t)) {
-		fprintf(stderr, "conn_handler_main(): buf too small");
-		fflush(stderr);
-		raise(SIGSEGV);
-	}
-
-	/* main event loop */
-	for (;;) {
-		poll_read(fds, nfds);
-
-		/* pipes */
-		if (fds[0].revents & POLLIN) {
-			handle_data_ready(ctube, fds, nfds);
-		}
-		if (fds[1].revents & POLLIN) {
-			handle_new_conn(&fds, &nfds, buf);
-		}
-
-		/* connections */
-		for (nfds_t i = 2; i < nfds; i++) {
-			if (fds[i].revents == 0) {
-				continue;
-			}
-
-			if (fds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-				handle_bad_conn(fds, &nfds, i);
-				continue;
-			}
-
-			if (fds[i].revents & POLLIN) {
-				handle_income(fds[i].fd);
-			}
-		}
-	}
-
-	/* cleanup */
-	free(fds);
-out_no_fds:
 	return NULL;
 }
 
@@ -230,14 +129,6 @@ static int conn_handler_init(struct ws_ctube *ctube, int serv_pipefd)
 	return pthread_create(&ctube->_conn_tid, NULL, conn_handler_main, (void *)&td);
 }
 
-struct serv_td {
-	int port;
-	struct ws_ctube *ctube;
-
-	int serv_stat;
-	pthread_barrier_t server_ready;
-};
-
 static int bind_serv(int serv_sock, int port) {
 	struct sockaddr_in sa;
 	sa.sin_family = AF_INET;
@@ -246,85 +137,82 @@ static int bind_serv(int serv_sock, int port) {
 	return bind(serv_sock, (struct sockaddr *)&sa, sizeof(sa));
 }
 
+static void serve_forever(int serv_sock)
+{
+	for (;;) {
+		int conn = accept(serv_sock, NULL, NULL);
+	}
+}
+
+struct serv_main_arg {
+	int port;
+	struct ws_ctube *ctube;
+
+	int serv_stat;
+	pthread_barrier_t server_ready;
+};
+
+struct serv_cleanup_arg {
+	int serv_sock;
+};
+
+static void *serv_cleanup(void *arg)
+{
+	int serv_sock = ((struct serv_cleanup_arg *)arg)->serv_sock;
+	close(serv_sock);
+	return NULL;
+}
+
 /** main for server */
 static void *serv_main(void *arg)
 {
-	int port = ((struct serv_td *)arg)->port;
-	struct ws_ctube *ctube = ((struct serv_td *)arg)->ctube;
-	int *serv_stat = &((struct serv_td *)arg)->serv_stat;
-	pthread_barrier_t *server_ready = &((struct serv_td *)arg)->server_ready;
+	int port = ((struct serv_main_arg *)arg)->port;
+	struct ws_ctube *ctube = ((struct serv_main_arg *)arg)->ctube;
+	int *serv_stat = &((struct serv_main_arg *)arg)->serv_stat;
+	pthread_barrier_t *server_ready = &((struct serv_main_arg *)arg)->server_ready;
 
 	/* create server socket */
 	int serv_sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (serv_sock == -1) {
 		perror(NULL);
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
 		goto out_nosock;
 	}
 	int true = 1;
 	if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &true,
 		   sizeof(int)) == -1) {
 		perror(NULL);
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
-		goto out_nopipe;
+		goto out_err;
 	}
 
 	/* set server socket address/port */
 	if (bind_serv(serv_sock, port) == -1) {
 		perror(NULL);
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
-		goto out_nopipe;
+		goto out_err;
 	}
 
 	/* set listening */
 	if (listen(serv_sock, 1) == -1) {
 		perror(NULL);
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
-		goto out_nopipe;
-	}
-
-	/* setup client connection handler */
-	/* we use this pipe to communicate new connections to the client
-	 * connection handler thread */
-	int serv_pipefd[2];
-	if (pipe(serv_pipefd) != 0) {
-		perror(NULL);
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
-		goto out_nopipe;
-	}
-	if (conn_handler_init(ctube, serv_pipefd[0]) != 0) {
-		*serv_stat = -1;
-		pthread_barrier_wait(server_ready);
-		goto out;
+		goto out_err;
 	}
 
 	/* success */
 	*serv_stat = 0;
+	struct serv_cleanup_arg serv_cleanup_arg = {
+		.serv_sock = serv_sock
+	};
+	pthread_cleanup_push(serv_cleanup, (void *)&serv_cleanup_arg);
 	pthread_barrier_wait(server_ready);
+	serv_forever();
+	pthread_cleanup_pop(0);
+	pthread_exit(0); // not necessary but just for aesthetics: thread exits only by being cancelled
 
-	/* serve forever */
-	new_conn_t conn;
-	char buf[WS_CTUBE_BUFLEN];
-	for (;;) {
-		/* new connection fd are written through the pipe to connection
-		 * handler thread */
-		conn = accept(serv_sock, NULL, NULL);
-		memcpy(buf, &conn, sizeof(new_conn_t));
-		write(serv_pipefd[1], buf, sizeof(new_conn_t));
-	}
-
-	/* cleanup */
-out:
-	close(serv_pipefd[0]);
-	close(serv_pipefd[1]);
-out_nopipe:
+	/* code doesn't get here unless error */
+out_err:
 	close(serv_sock);
 out_nosock:
+	*serv_stat = -1;
+	pthread_barrier_wait(server_ready);
 	return NULL;
 }
 
@@ -336,32 +224,31 @@ static int syncprim_init(struct ws_ctube *ctube)
 	pthread_mutex_init(&ctube->_mutex, &attr);
 	pthread_mutexattr_destroy(&attr);
 
-	pthread_cond_init(&ctube->_data_ready_cond);
-
+	pthread_cond_init(&ctube->_data_ready_cond, NULL);
 	return 0;
 }
 
 int ws_ctube_init(struct ws_ctube *ctube, int port)
 {
 	if (syncprim_init(ctube) != 0) {
-		fprintf(stderr, "syncprim_init(): failed");
+		fprintf(stderr, "syncprim_init(): failed\n");
 		fflush(stderr);
 		return -1;
 	}
 
-	struct serv_td serv_td;
-	serv_td.port = port;
-	serv_td.ctube = ctube;
-	serv_td.serv_stat = 0;
-	pthread_barrier_init(&serv_td.server_ready, NULL, 2);
+	struct serv_main_arg serv_main_arg;
+	serv_main_arg.port = port;
+	serv_main_arg.ctube = ctube;
+	serv_main_arg.serv_stat = 0;
+	pthread_barrier_init(&serv_main_arg.server_ready, NULL, 2);
 
-	if (pthread_create(&ctube->_serv_tid, NULL, serv_main, (void *)&serv_td) != 0) {
-		pthread_barrier_destroy(&serv_td.server_ready);
+	if (pthread_create(&ctube->_serv_tid, NULL, serv_main, (void *)&serv_main_arg) != 0) {
+		pthread_barrier_destroy(&serv_main_arg.server_ready);
 		return -1;
 	} else {
-		pthread_barrier_wait(&serv_td.server_ready);
-		pthread_barrier_destroy(&serv_td.server_ready);
-		return serv_td.serv_stat;
+		pthread_barrier_wait(&serv_main_arg.server_ready);
+		pthread_barrier_destroy(&serv_main_arg.server_ready);
+		return serv_main_arg.serv_stat;
 	}
 }
 
