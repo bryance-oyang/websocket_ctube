610ab7a11fc476a59e93df10adda579ef1f7fdf9
Bryance Oyang <bryance@ducky>
2023-01-29 18:37:25 -0800

    main improvements

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "ws_ctube.h"
 
@@ -7,7 +9,13 @@ int main()
 	struct ws_ctube ctube;
 	ws_ctube_init(&ctube, 9999, 10);
 
-	sleep(10);
+	for (int i = 0; i < 20; i++) {
+		ctube.data = malloc(4096);
+		ctube.data_size = snprintf(ctube.data, 4096, "hello, world! %d\n", i);
+		ws_ctube_broadcast(&ctube);
+		free(ctube.data);
+		sleep(1);
+	}
 
 	ws_ctube_destroy(&ctube);
 	return 0;
