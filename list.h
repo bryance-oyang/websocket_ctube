ab121dc87d993105439cb691b1735286e41c9fda
Bryance Oyang <bryance@ducky>
2023-02-07 02:16:37 -0800

    fix list

--- list.h
+++ list.h
@@ -18,7 +18,6 @@ static int list_node_init(struct list_node *node)
 	node->prev = NULL;
 	node->next = NULL;
 	pthread_mutex_init(&node->mutex, NULL);
-
 	return 0;
 }
 
@@ -31,112 +30,144 @@ static void list_node_destroy(struct list_node *node)
 
 struct list {
 	struct list_node head;
-	struct list_node tail;
+	int len;
+	pthread_mutex_t mutex;
 };
 
 static int list_init(struct list *l)
 {
 	list_node_init(&l->head);
-	list_node_init(&l->tail);
-
-	l->head.next = &l->tail;
-	l->tail.prev = &l->head;
-
+	l->head.next = &l->head;
+	l->head.prev = &l->head;
+	l->len = 0;
+	pthread_mutex_init(&l->mutex, NULL);
 	return 0;
 }
 
 static void list_destroy(struct list *l)
 {
 	list_node_destroy(&l->head);
-	list_node_destroy(&l->tail);
+	pthread_mutex_destroy(&l->mutex);
 }
 
-static void _list_link(struct list_node *a, struct list_node *b, struct list_node *c)
+static void _list_add_after(struct list_node *prior, struct list_node *new_node)
 {
-	a->next = b;
-	b->prev = a;
-	b->next = c;
-	c->prev = b;
+	new_node->prev = prior;
+	new_node->next = prior->next;
+	prior->next->prev = new_node;
+	prior->next = new_node;
 }
 
-static void list_push_front(struct list *l, struct list_node *node)
+static void _list_node_unlink(struct list_node *node)
 {
-	pthread_mutex_lock(&l->head.mutex);
+	node->next->prev = node->prev;
+	node->prev->next = node->next;
+	node->next = NULL;
+	node->prev = NULL;
+}
+
+static void list_node_push_front(struct list *l, struct list_node *node)
+{
 	pthread_mutex_lock(&node->mutex);
-	pthread_mutex_lock(&l->head.next->mutex);
+	pthread_mutex_lock(&l->mutex);
 
-	_list_link(&l->head, node, l->head.next);
-
-	pthread_mutex_unlock(&l->head.next->mutex);
-	pthread_mutex_unlock(&node->mutex);
-	pthread_mutex_unlock(&l->head.mutex);
-}
-
-static struct list_node *list_pop_front(struct list *l)
-{
-	struct list_node
-	pthread_mutex_lock(&l->head.mutex);
-	struct list_node *node = l->head.next;
-	if (node == &l->tail) {
-		pthread_mutex_unlock(&l->head.mutex);
-		return NULL;
+	if (node->next != NULL || node->prev != NULL) {
+		goto out;
 	}
-	pthread_mutex_lock(&node->mutex);
-	pthread_mutex_lock(&node->next->mutex);
+	_list_add_after(&l->head, node);
+	l->len++;
 
-	pthread_mutex_unlock(&node->next->mutex);
+out:
+	pthread_mutex_unlock(&l->mutex);
 	pthread_mutex_unlock(&node->mutex);
-	pthread_mutex_unlock(&l->head.mutex);
-	return node;
 }
 
-static void list_push_back(struct list *l, struct list_node *node)
+static void list_node_push_back(struct list *l, struct list_node *node)
 {
-	pthread_mutex_lock(&l->tail.prev->mutex);
 	pthread_mutex_lock(&node->mutex);
-	pthread_mutex_lock(&l->)
-	_list_link(l->tail.prev, node, &l->tail);
+	pthread_mutex_lock(&l->mutex);
+
+	if (node->next != NULL || node->prev != NULL) {
+		goto out;
+	}
+	_list_add_after(&l->head.prev, node);
+	l->len++;
+
+out:
+	pthread_mutex_unlock(&l->mutex);
+	pthread_mutex_unlock(&node->mutex);
 }
 
-static struct list_node *list_pop_back(struct list *l)
+static struct list_node *list_node_lockpop_front(struct list *l)
 {
-	struct list_node *node = l->tail.prev;
-	if (node == &l->head) {
-		return NULL;
+	struct list_node *front;
+
+	/* optimistically lock front mutex but check front is still front */
+	for (;;) {
+		front = l->head.next;
+
+		pthread_mutex_lock(&front->mutex);
+		pthread_mutex_lock(&l->mutex);
+		if (front == &l->head) {
+			pthread_mutex_unlock(&l->mutex);
+			pthread_mutex_unlock(&front->mutex);
+			return NULL;
+		} else if (front != l->head.next) {
+			pthread_mutex_unlock(&l->mutex);
+			pthread_mutex_unlock(&front->mutex);
+			continue;
+		} else {
+			break;
+		}
 	}
 
-	list_node_unlink(node);
-	return node;
+	_list_node_unlink(front);
+	l->len--;
+	pthread_mutex_unlock(&l->mutex);
+}
+
+static struct list_node *list_node_lockpop_back(struct list *l)
+{
+	struct list_node *back;
+
+	/* optimistically lock back mutex but check back is still back */
+	for (;;) {
+		back = l->head.prev;
+
+		pthread_mutex_lock(&back->mutex);
+		pthread_mutex_lock(&l->mutex);
+		if (back == &l->head) {
+			pthread_mutex_unlock(&l->mutex);
+			pthread_mutex_unlock(&back->mutex);
+			return NULL;
+		} else if (back != l->head.prev) {
+			pthread_mutex_unlock(&l->mutex);
+			pthread_mutex_unlock(&back->mutex);
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	_list_node_unlink(back);
+	l->len--;
+	pthread_mutex_unlock(&l->mutex);
 }
 
 #define list_for_each(list, node) \
 	for ( \
-	pthread_mutex_lock(&(list)->head.mutex), \
-	node = (list)->head.next, \
-	pthread_mutex_lock(&node->mutex), \
-	pthread_mutex_unlock(&(list)->head.mutex) \
-	; \
-	node != &(list)->tail \
-	|| (pthread_mutex_unlock(&node->mutex) && 0) \
-	; \
-	node = node->next, \
-	pthread_mutex_lock(&node->mutex), \
-	pthread_mutex_unlock(&node->prev->mutex))
+	node = (list)->head.next; \
+	node != &((list)->head); \
+	node = node->next)
 
-#define list_for_each_entry(list, node, entry, member) \
+#define list_for_each_entry(list, entry, member) \
 	for ( \
-	pthread_mutex_lock(&(list)->head.mutex), \
-	node = (list)->head.next, \
-	pthread_mutex_lock(&node->mutex), \
-	pthread_mutex_unlock(&(list)->head.mutex), \
-	entry = container_of(node, typeof(*entry), member) \
-	; \
-	node != &(list)->tail \
-	|| (pthread_mutex_unlock(&node->mutex) && 0)\
-	; \
-	node = node->next, \
-	pthread_mutex_lock(&node->mutex), \
-	pthread_mutex_unlock(&node->prev->mutex), \
-	entry = container_of(node, typeof(*entry), member))
+	entry = (list)->head.next != &(list)->head ? \
+	container_of((list)->head.next, typeof(*entry), member) : \
+	NULL; \
+	entry != NULL; \
+	entry = entry->member->next != &(list)->head ? \
+	container_of(entry->member->next, typeof(*entry), member) : \
+	NULL)
 
 #endif /* LIST_H */
