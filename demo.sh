9c192a0d9fcb5b41aa2bd488cb8d1bac2ffd5e25
Bryance Oyang <bryance@ducky>
2023-02-16 01:18:43 -0800

    mv html demo file

--- demo.sh
+++ demo.sh
@@ -6,5 +6,6 @@ cd example_heat_equation
 make clean
 make
 
+cd ..
 printf "\n\n\n\n\n\n\e[1;37mOpen a browser to \e[1;36mhttp://localhost:9736/example_heat_equation.html\e[1;37m for demo\e[0m\n"
-(trap 'kill 0' SIGINT; python -m http.server 9736 &>/dev/null & ./a.out)
+(trap 'kill 0' SIGINT; python -m http.server 9736 &>/dev/null & ./example_heat_equation/a.out)
