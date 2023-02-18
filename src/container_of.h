2a58fed654f716f0520dd71130f92f011dc99fa9
Bryance Oyang <bryance@ducky>
2023-02-17 18:09:35 -0800

    container_of doc

--- src/container_of.h
+++ src/container_of.h
@@ -1,6 +1,21 @@
 #ifndef WS_CTUBE_CONTAINER_OF_H
 #define WS_CTUBE_CONTAINER_OF_H
 
+/**
+ * get the container of ptr which is a member of a struct
+ *
+ * Example:
+ *
+ * struct a {
+ * 	struct b bmember;
+ * 	struct c cmember;
+ * };
+ *
+ * struct a *a_ptr;
+ * struct c *c_ptr = &a_ptr->cmember;
+ *
+ * a_ptr == ws_ctube_container_of(c_ptr, struct a, cmember) // true
+ */
 #define ws_ctube_container_of(ptr, type, member) ({ \
 	typeof(((type *)0)->member) *_container_of_ptr = ptr; \
 	((type *)((char *)(_container_of_ptr) - offsetof(type, member)));})
